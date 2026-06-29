16a1e675b4c7ce2cb3c82dae53b3dc8f28df399c
hassanmudabber3-ai <hassanmudabber3@gmail.com>
2026-06-29 15:48:06 +0430

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,44 @@
 import os
 
 TOKEN = os.getenv("TOKEN")
+
+
+MINING_RATES = [
+
+    {
+        "min":0,
+        "max":10000,
+        "rate":2
+    },
+
+    {
+        "min":10000,
+        "max":100000,
+        "rate":1.2
+    },
+
+    {
+        "min":100000,
+        "max":1000000,
+        "rate":0.5
+    },
+
+    {
+        "min":1000000,
+        "max":10000000,
+        "rate":0.2
+    }
+
+]
+
+
+def get_mining_rate(users):
+
+    for r in MINING_RATES:
+
+        if users >= r["min"] and users < r["max"]:
+
+            return r["rate"]
+
+
+    return 0.1
