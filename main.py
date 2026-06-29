9e46b37298165eb7ba5b407775a2c64af62f9a08
hassanmudabber3-ai <hassanmudabber3@gmail.com>
2026-06-29 17:55:16 +0430

    Update main.py

--- main.py
+++ main.py
@@ -1,139 +1,54 @@
 import os
-import asyncio
 
-from flask import Flask
-from threading import Thread
-
-from telegram import (
-    InlineKeyboardButton,
-    InlineKeyboardMarkup,
-    WebAppInfo
-)
-
+from telegram import Update
 from telegram.ext import (
     Application,
-    CommandHandler
+    CommandHandler,
+    ContextTypes
 )
-
 
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-WEB_APP_URL = "https://silkcoin-network.onrender.com"
 
 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
-# =====================
-# RENDER SERVER
-# =====================
-
-server = Flask(__name__)
-
-
-@server.route("/")
-def home():
-
-    return "Silkcoin Bot Running"
-
-
-
-def run_web():
-
-    port = int(os.environ.get("PORT",10000))
-
-    server.run(
-        host="0.0.0.0",
-        port=port
-    )
-
-
-
-
-
-# =====================
-# TELEGRAM
-# =====================
-
-
-async def start(update, context):
-
-
-    keyboard = [
-
-        [
-
-            InlineKeyboardButton(
-
-                "🚀 Open Silkcoin Network Coin",
-
-                web_app=WebAppInfo(
-
-                    url=WEB_APP_URL
-
-                )
-
-            )
-
-        ]
-
-    ]
-
-
+    print("START COMMAND RECEIVED")
 
     await update.message.reply_text(
-
-        "💎 Silkcoin Network\n\n"
-        "Welcome 🚀\n\n"
-        "Open Silkcoin:",
-
-        reply_markup=InlineKeyboardMarkup(keyboard)
-
+        "💎 Silkcoin Bot is alive 🚀"
     )
 
 
 
+def main():
 
 
-
-async def run_bot():
+    print("TOKEN CHECK:", bool(BOT_TOKEN))
 
 
     app = Application.builder().token(
-
         BOT_TOKEN
-
     ).build()
 
 
 
     app.add_handler(
-
         CommandHandler(
-
             "start",
-
             start
-
         )
-
     )
 
 
 
-    print("🪙 Telegram Bot Started")
+    print(
+        "BOT RUNNING"
+    )
 
 
-
-    await app.initialize()
-
-    await app.start()
-
-    await app.updater.start_polling()
-
-
-
-    await asyncio.Event().wait()
-
+    app.run_polling()
 
 
 
@@ -141,12 +56,4 @@ async def run_bot():
 
 if __name__ == "__main__":
 
-
-    Thread(
-        target=run_web
-    ).start()
-
-
-    asyncio.run(
-        run_bot()
-    )
+    main()
