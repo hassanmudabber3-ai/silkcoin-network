bac5deff56823f0672c9713ed7c5819026b68786
hassanmudabber3-ai <hassanmudabber3@gmail.com>
2026-06-29 17:58:24 +0430

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import os
+import threading
 
-from telegram import Update
+from flask import Flask
+
+from telegram import (
+    Update,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    WebAppInfo
+)
+
 from telegram.ext import (
     Application,
     CommandHandler,
@@ -8,52 +17,145 @@ from telegram.ext import (
 )
 
 
+
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
+WEB_APP_URL = "https://silkcoin-network.onrender.com"
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
-    print("START COMMAND RECEIVED")
 
-    await update.message.reply_text(
-        "💎 Silkcoin Bot is alive 🚀"
+# ==================
+# RENDER SERVER
+# ==================
+
+app_web = Flask(__name__)
+
+
+@app_web.route("/")
+def home():
+
+    return "Silkcoin Bot Running"
+
+
+
+
+def run_web():
+
+    port = int(
+        os.environ.get(
+            "PORT",
+            10000
+        )
+    )
+
+    app_web.run(
+
+        host="0.0.0.0",
+
+        port=port
+
     )
 
 
 
-def main():
 
 
-    print("TOKEN CHECK:", bool(BOT_TOKEN))
+# ==================
+# TELEGRAM BOT
+# ==================
 
 
-    app = Application.builder().token(
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+
+    keyboard = [
+
+        [
+
+            InlineKeyboardButton(
+
+                "🚀 Open Silkcoin Network Coin",
+
+                web_app=WebAppInfo(
+
+                    url=WEB_APP_URL
+
+                )
+
+            )
+
+        ]
+
+    ]
+
+
+
+    await update.message.reply_text(
+
+        "💎 Silkcoin Network\n\n"
+        "Welcome 🚀\n\n"
+        "Click below to open Silkcoin:",
+
+        reply_markup=InlineKeyboardMarkup(
+            keyboard
+        )
+
+    )
+
+
+
+
+
+def run_bot():
+
+
+    bot = Application.builder().token(
+
         BOT_TOKEN
+
     ).build()
 
 
 
-    app.add_handler(
+    bot.add_handler(
+
         CommandHandler(
+
             "start",
+
             start
+
         )
+
     )
 
 
 
     print(
-        "BOT RUNNING"
+        "🪙 Silkcoin Telegram Bot Started"
     )
 
 
-    app.run_polling()
+    bot.run_polling()
 
 
 
 
+
+# ==================
+# START
+# ==================
 
 if __name__ == "__main__":
 
-    main()
+
+    threading.Thread(
+
+        target=run_web
+
+    ).start()
+
+
+
+    run_bot()
