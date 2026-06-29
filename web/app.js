daf13ecc4cb4feb9dbaa14b10a6a50f44ff07d95
hassanmudabber3-ai <hassanmudabber3@gmail.com>
2026-06-29 06:38:19 +0430

    Update app.js

--- web/app.js
+++ web/app.js
@@ -1,1 +1,54 @@
+const tg = window.Telegram.WebApp;
 
+tg.expand();
+
+document.getElementById("wallet").innerHTML = "Loading...";
+
+fetch("/api/user?id=" + tg.initDataUnsafe.user.id)
+.then(r=>r.json())
+.then(data=>{
+
+    document.getElementById("wallet").innerHTML=data.wallet_id;
+
+    document.getElementById("balance").innerHTML=data.balance+" SCN";
+
+});
+
+
+function startMining(){
+
+    fetch("/api/mine/start");
+
+    document.getElementById("message").innerHTML="Mining Started";
+
+}
+
+
+function boost(){
+
+    fetch("/api/ad");
+
+    document.getElementById("message").innerHTML="1 Spin Added";
+
+}
+
+
+function spin(){
+
+    fetch("/api/spin")
+    .then(r=>r.json())
+    .then(data=>{
+
+        document.getElementById("message").innerHTML=
+        "You won "+data.reward+" SCN";
+
+    });
+
+}
+
+
+function friends(){
+
+    alert("Coming Soon");
+
+}
