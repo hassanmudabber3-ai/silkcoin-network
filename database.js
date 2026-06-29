caa7d0bdcc7547dee361a922f6ce1a467bc4c2ee
hassanmudabber3-ai <hassanmudabber3@gmail.com>
2026-06-29 08:06:47 +0430

    Update database.js

--- database.js
+++ database.js
@@ -1,1 +1,32 @@
+const sqlite3 = require("sqlite3").verbose();
 
+
+const db = new sqlite3.Database(
+"silkcoin.db"
+);
+
+
+
+db.run(`
+
+CREATE TABLE IF NOT EXISTS users (
+
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+telegram_id TEXT UNIQUE,
+
+wallet TEXT UNIQUE,
+
+balance REAL DEFAULT 100,
+
+referrer TEXT,
+
+referrals INTEGER DEFAULT 0
+
+)
+
+`);
+
+
+
+module.exports = db;
